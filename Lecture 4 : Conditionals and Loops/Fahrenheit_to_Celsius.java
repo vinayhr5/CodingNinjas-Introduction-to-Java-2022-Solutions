3a22e7d4d0bc7be716a0f259e041a77c824f3cd4
Vinay Ahari <vinayhr5@gmail.com>
2022-06-28 20:45:39 +0530

    Create Fahrenheit_to_Celsius.java

---
+++ Lecture 4 : Conditionals and Loops/Fahrenheit_to_Celsius.java
@@ -0,0 +1,61 @@
+/*
+ Fahrenheit to Celsius Table
+Send Feedback
+Given three values - Start Fahrenheit Value (S), End Fahrenheit value (E) and Step Size (W), you need to convert all Fahrenheit values from Start to End at the gap of W, into their corresponding Celsius values and print the table.
+Input Format :
+
+3 integers - S, E and W respectively 
+
+Output Format :
+
+Fahrenheit to Celsius conversion table. One line for every Fahrenheit and corresponding Celsius value. The Fahrenheit value and its corresponding Celsius value should be separate by single space.
+
+Constraints :
+
+0 <= S <= 90
+S <= E <=  900
+0 <= W <= 80 
+
+Sample Input 1:
+
+0 
+100 
+20
+
+Sample Output 1:
+
+0   -17
+20  -6
+40  4
+60  15
+80  26
+100 37
+*/
+
+import java.util.Scanner;
+public class Solution {
+
+
+	public static void main(String[] args) {
+		
+		/* Your class should be named Solution.
+	 	* Read input as specified in the question.
+	 	* Print output as specified in the question.
+		*/
+        int start, end, step;
+		Scanner sc = new Scanner(System.in);
+		start = sc.nextInt();
+		end = sc.nextInt();
+		step = sc.nextInt();
+		
+		while(start<=end){
+		    
+		    int celsius = ((start-32)*5)/9;
+		    System.out.println(start+" "+celsius);
+		    start = start+step;
+		}
+
+		
+	}
+
+}
