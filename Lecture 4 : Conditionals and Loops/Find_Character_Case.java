19de7b99caa75a371136b019ba5ab03d68b09462
Vinay Ahari <vinayhr5@gmail.com>
2022-06-28 20:43:23 +0530

    Create Find_Character_Case.java

---
+++ Lecture 4 : Conditionals and Loops/Find_Character_Case.java
@@ -0,0 +1,47 @@
+/*
+ Find Character Case
+Send Feedback
+Write a program that takes a character as input and prints either 1, 0 or -1 according to the following rules.
+1, if the character is an uppercase alphabet (A - Z)
+0, if the character is a lowercase alphabet (a - z)
+-1, if the character is not an alphabet
+Input format :
+
+Single Character
+
+Output format :
+
+1 or 0 or -1
+
+Constraints :
+
+Input can be any character
+
+Sample Input 1 :
+
+v
+
+Sample Output 1 :
+
+0
+
+*/
+
+import java.util.Scanner;
+public class Solution {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		char input = sc.next().charAt(0);
+
+		
+		if (input >= 'A' && input <= 'Z'){
+		    System.out.println("1");
+		} else if (input >= 'a' && input <= 'z') {
+		    System.out.println("0");
+		}else {
+		    System.out.println("-1");
+		}
+
+    }
+}
